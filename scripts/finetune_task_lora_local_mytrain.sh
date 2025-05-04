b63f41c38b4ecf4e7e91e389fb6094bcbc36147d
WaterHyacinthInNANHU <mingxuanyan99@outlook.com>
2025-05-03 17:14:44 -0700

    optimized training script

--- scripts/finetune_task_lora_local_mytrain.sh
+++ scripts/finetune_task_lora_local_mytrain.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 set -x
 
-export GPUS_PER_NODE=3
-export CUDA_VISIBLE_DEVICES=4,5,6
-EPOCH=2
+#!/bin/bash
+set -e -x
 
-SAVE_PATH=llava_llama3_racer_alltask_lora_debug
-MODEL_PATH=$1
+######### Edit this part #########
+export CUDA_VISIBLE_DEVICES=5,6,7
+####################################
+FINETUNE_DATASET=$1
+FINETUNE_MODEL_NAME=$2
+BASE_MODEL_PATH=../models/llama3-llava-next-8b
+
+
+# preprocessing dataset
+if [ ! -d "$FINETUNE_DATASET" ]; then
+	echo "require a valid directory as the first argument"
+	exit 1
+fi
+
+[ -L "augmented_rlbench" ] && rm augmented_rlbench
+ln -s $FINETUNE_DATASET augmented_rlbench
+if [ ! -d "playground" ]; then
+	rm playground
+fi
+python gather_racer_llava_data.py
+
+
+# train
+GPUS_PER_NODE=$(echo $CUDA_VISIBLE_DEVICES | awk -F, '{print NF}')
+echo "GPUS_PER_NODE: $GPUS_PER_NODE"
+EPOCH=2
 
 root_path=$(pwd)
 
@@ -14,7 +37,7 @@ torchrun --nnodes 1 --nproc_per_node $GPUS_PER_NODE --node_rank 0 --master_addr 
     llava/train/my_train.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path $MODEL_PATH \
+    --model_name_or_path $BASE_MODEL_PATH \
     --version llava_llama_3_racer \
     --data_path $root_path/playground/racer_llava_data/all_tasks.json \
     --image_folder $root_path \
@@ -28,7 +51,7 @@ torchrun --nnodes 1 --nproc_per_node $GPUS_PER_NODE --node_rank 0 --master_addr 
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir checkpoints/${SAVE_PATH} \
+    --output_dir checkpoints/${FINETUNE_MODEL_NAME} \
     --num_train_epochs $EPOCH \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
@@ -48,5 +71,5 @@ torchrun --nnodes 1 --nproc_per_node $GPUS_PER_NODE --node_rank 0 --master_addr 
     --dataloader_num_workers 3 \
     --lazy_preprocess True \
     --report_to tensorboard \
-    --run_name ${SAVE_PATH} \
+    --run_name ${FINETUNE_MODEL_NAME} \
     --lang_level simple
